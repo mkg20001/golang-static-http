9a3435182c0dfbb75ea3c5cd00a8df214e0f02a9
Maciej Krüger <mkg20001@gmail.com>
2016-05-02 17:16:26 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "net/http"
 
 func main() {
-        panic(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+        panic(http.ListenAndServe(":8448", http.FileServer(http.Dir("."))))
 }
 
